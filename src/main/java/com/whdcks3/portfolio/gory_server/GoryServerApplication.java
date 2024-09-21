d5895dd8d1a49f956878c6432a5a9a8c865901ea
JongChan <sj012944@gmail.com>
2024-09-21 21:30:35 +0900

    initialize

---
+++ src/main/java/com/whdcks3/portfolio/gory_server/GoryServerApplication.java
@@ -0,0 +1,13 @@
+package com.whdcks3.portfolio.gory_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whdcks3/portfolio/gory_server/GoryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whdcks3.portfolio.gory_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
